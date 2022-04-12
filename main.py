42d8d096d55a8f0908aec8332534ac8c1d204683
SkrighYZ <zhang.yipeng@columbia.edu>
2022-04-11 23:16:23 -0400

    .

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def main():
 
 	parser.add_argument('--seed', type=int, default=10)
 	parser.add_argument('--num_workers', type=int, default=4)
-	parser.add_argument('--print-freq', default=5, type=int, metavar='N', help='print frequency')
+	parser.add_argument('--print-freq', default=1, type=int, metavar='N', help='print frequency')
 
 	parser.add_argument('--projector', default='2048-2048', type=str, metavar='MLP', help='projector MLP')
 	parser.add_argument('--lambd', default=0.0051, type=float, metavar='L', help='weight on off-diagonal terms')
