3cf2b1977b8ff7ca1e20654199f7b9d04d88633e
SkrighYZ <zhang.yipeng@columbia.edu>
2022-04-13 20:02:58 -0400

    debug

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def train(args, model, device='cuda:0'):
 		for step, ((y1, y2), _) in enumerate(train_loader, start=epoch*len(train_loader)):
 
 			pickle.save(y1, open('../y1.pkl', 'wb'))
-			pickle.save(y2, open('../y1.pkl', 'wb'))
+			pickle.save(y2, open('../y2.pkl', 'wb'))
 			break
 			
 			if replay_sampler:
