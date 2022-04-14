1d3f12b79d65110d09c7e759dfa9d89046e4e81d
SkrighYZ <zhang.yipeng@columbia.edu>
2022-04-14 03:50:42 -0400

    .

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@
 python main.py \
 	--order iid \
 	--model sliding_bt \
-	--batch_size 256 \
-	--epochs 50 \
-	--warmup_epochs 5 \
-	--learning_rate_weights 0.2 \
+	--batch_size 128 \
+	--buffer_size 256 \
+	--epochs 20 \
+	--warmup_epochs 2 \
+	--learning_rate_weights 0.3 \
 	--learning_rate_biases 0.005 \
-	--print_freq 200 \
-	--save_freq 10 \
+	--print_freq 400 \
+	--save_freq 5 \
 	--projector 2048-2048 \
 	--save_dir ../models/sliding_bt
