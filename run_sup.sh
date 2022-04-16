31ce57a5dd34fdd89063fdb5027170a3ddd72ac3
SkrighYZ <zhang.yipeng@columbia.edu>
2022-04-15 22:18:03 -0400

    .

--- run_sup.sh
+++ run_sup.sh
@@ -3,7 +3,7 @@
 
 python main.py \
 	--mode train \
-	--order iid \
+	--order instance \
 	--model sliding_supervised \
 	--batch_size 256 \
 	--buffer_size 256 \
@@ -17,4 +17,22 @@ python main.py \
 	--save_freq 1 \
 	--projector 2048-2048 \
 	--num_classes 51 \
-	--save_dir ../models/iid_sup
+	--save_dir ../models/instance_sup
+
+# python main.py \
+# 	--mode train \
+# 	--order iid \
+# 	--model sliding_supervised \
+# 	--batch_size 256 \
+# 	--buffer_size 256 \
+# 	--epochs 10 \
+# 	--warmup_epochs 1 \
+# 	--learning_rate_weights 0.2 \
+# 	--learning_rate_biases 0.2 \
+# 	--lr_decay 1 \
+# 	--weight_decay 1e-4 \
+# 	--print_freq 400 \
+# 	--save_freq 1 \
+# 	--projector 2048-2048 \
+# 	--num_classes 51 \
+# 	--save_dir ../models/iid_sup
