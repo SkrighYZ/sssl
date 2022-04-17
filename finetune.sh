c3990232856c47fbe7b3c074ebb6e74516640f1b
SkrighYZ <zhang.yipeng@columbia.edu>
2022-04-16 22:01:05 -0400

    .

--- finetune.sh
+++ finetune.sh
@@ -3,10 +3,10 @@
 
 python finetune.py \
 	--batch_size 256 \
-	--epochs 100 \
+	--epochs 80 \
 	--learning_rate 0.3 \
 	--lr_decay 0.1 \
-	--decay_epochs 40 80 \
+	--decay_epochs 30 60 \
 	--weight_decay 1e-4 \
 	--eval_freq 10 \
-	--ckpt_dir ../models/instance_bt
+	--ckpt_dir ../models/iid_sup
