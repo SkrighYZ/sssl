05b57c5fc048ca180d18251d8175e768e3e184d6
SkrighYZ <zhang.yipeng@columbia.edu>
2022-04-20 16:21:39 -0400

    .

--- finetune.sh
+++ finetune.sh
@@ -6,7 +6,7 @@ python finetune.py \
 	--epochs 50 \
 	--learning_rate 0.2 \
 	--lr_decay 0.1 \
-	--decay_epochs 20 40 \
+	--decay_epochs 20 42 \
 	--weight_decay 1e-4 \
 	--eval_freq 5 \
 	--ckpt_dir ../models/reservoir_bt_stm1_b128
