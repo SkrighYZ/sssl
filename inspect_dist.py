15d11d5084b2ed4d90a8d4f8c0d6bfc42055c955
SkrighYZ <zhang.yipeng@columbia.edu>
2022-05-09 01:22:17 -0400

    .

--- inspect_dist.py
+++ inspect_dist.py
@@ -2,12 +2,12 @@ import pickle
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
+import random
 
 plt.style.use('seaborn')
 
 save_dir = sys.argv[1]
 num_classes = 51
-
 
 samples = pickle.load(open('../sssl-ws/' + save_dir + '/samples.pkl', 'rb'))
 stm_batches = pickle.load(open('../sssl-ws/' + save_dir + '/stm_batches.pkl', 'rb'))
@@ -21,6 +21,12 @@ for s in samples:
 		curr_clip += 1
 	clip_labels.append(curr_clip)
 class_labels = [s[0] for s in samples]
+
+num_samples = (stm_batches.shape[0]-1) * (stm_batches.shape[1]+ltm_batches.shape[1])
+iid_batches = list(range(num_samples))
+random.shuffle(iid_batches)
+iid_batches = np.array(iid_batches, dtype=int).reshape(
+		(stm_batches.shape[0]-1, stm_batches.shape[1]+ltm_batches.shape[1]))
 
 
 stm_sample_dist = []
@@ -37,6 +43,9 @@ ltm_clip_per_batch = []
 
 total_class_per_batch = []
 total_clip_per_batch = []
+
+iid_class_per_batch = []
+iid_clip_per_batch = []
 
 for b_i in range(len(stm_batches)):
 
@@ -71,6 +80,16 @@ for b_i in range(len(stm_batches)):
 	total_class_per_batch.append(len(total_class_batch))
 	total_clip_per_batch.append(len(total_clip_batch))
 
+	if b_i == iid_batches.shape[0]: break
+
+	iid_class_batch = set()
+	iid_clip_batch = set()
+	for i in iid_batches[b_i]:
+		iid_class_batch.add(class_labels[i])
+		iid_clip_batch.add(clip_labels[i])
+	iid_class_per_batch.append(len(iid_class_batch))
+	iid_clip_per_batch.append(len(iid_clip_batch))
+
 
 
 
@@ -98,8 +117,9 @@ for b_i in range(len(stm_batches)):
 plt.plot(ltm_class_per_batch, label='LTM')
 plt.plot(stm_class_per_batch, label='STM')
 plt.plot(total_class_per_batch, label='Total')
-optimal = [51] * len(ltm_class_per_batch)
-plt.plot(optimal, ls='--', label='optimal')
+plt.plot(iid_class_per_batch, label='IID')
+upper_bound = [51] * len(ltm_class_per_batch)
+plt.plot(upper_bound, ls='--', label='Upper Bound')
 plt.ylim((0, 55))
 plt.ylabel('Num Classes Within Batch')
 plt.xlabel('Step')
@@ -110,8 +130,9 @@ plt.show()
 plt.plot(ltm_clip_per_batch, label='LTM')
 plt.plot(stm_clip_per_batch, label='STM')
 plt.plot(total_clip_per_batch, label='Total')
-optimal = [128] * len(ltm_clip_per_batch)
-plt.plot(optimal, ls='--', label='optimal')
+plt.plot(iid_clip_per_batch, label='IID')
+upper_bound = [128] * len(ltm_clip_per_batch)
+plt.plot(upper_bound, ls='--', label='Upper Bound')
 plt.ylim((0, 130))
 plt.ylabel('Num Clips Within Batch')
 plt.xlabel('Step')
