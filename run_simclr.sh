b5e058683a6d20bbef112556005d44748ad92546
SkrighYZ <zhang.yipeng@columbia.edu>
2022-05-09 00:47:50 -0400

    .

--- run_simclr.sh
+++ run_simclr.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+python main.py \
+  --mode train \
+	--order iid \
+	--model sliding_simclr \
+	--batch_size 256 \
+	--epochs 10 \
+	--warmup_epochs 1 \
+	--learning_rate_weights 0.3 \
+	--learning_rate_biases 0.005 \
+	--lr_decay 1 \
+	--print_freq 400 \
+	--save_freq 5 \
+	--projector 2048-2048 \
+	--save_dir ../models/iid_simclr_b256
+
 
 python main.py \
 	--mode train \
@@ -21,20 +36,5 @@ python main.py \
 	--save_dir ../models/r_simclr_s128_b128_sb64 \
 	--corrupt_rate 0.1 
 #	--use_boundary
-
-# python main.py \
-#   --mode train \
-# 	--order iid \
-# 	--model sliding_simclr \
-# 	--batch_size 256 \
-# 	--epochs 10 \
-# 	--warmup_epochs 1 \
-# 	--learning_rate_weights 0.3 \
-# 	--learning_rate_biases 0.005 \
-# 	--lr_decay 1 \
-# 	--print_freq 400 \
-# 	--save_freq 5 \
-# 	--projector 2048-2048 \
-# 	--save_dir ../models/iid_simclr_b256
 
 
