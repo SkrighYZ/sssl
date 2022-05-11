32c6f027f46ece7ed8e6bb2bfcb463c67a66c821
SkrighYZ <zhang.yipeng@columbia.edu>
2022-05-10 20:40:15 -0400

    .

--- run_bt2.sh
+++ run_bt2.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 python main.py \
 	--mode train \
 	--order instance \
 	--model reservoir_bt \
-	--stm_size 32 \
-	--stm_batch_size 16 \
+	--stm_size 128 \
+	--stm_batch_size 64 \
 	--stm_span 1500 \
 	--batch_size 128 \
 	--epochs 10 \
@@ -14,8 +16,10 @@ python main.py \
 	--print_freq 400 \
 	--save_freq 5 \
 	--projector 2048-2048 \
-	--save_dir ../models/r_bt_s32_b128_sb16 \
-	--corrupt_rate 0.1 
+	--save_dir ../models/r_mm_bt_s128_b128_sb64 \
+	--selection_policy min-replay \
+	--store_policy min-replay 
+#	--corrupt_rate 0.1 \
 #	--use_boundary
 
 python finetune.py \
@@ -26,15 +30,15 @@ python finetune.py \
 	--decay_epochs 20 42 \
 	--weight_decay 1e-4 \
 	--eval_freq 5 \
-	--ckpt_dir ../models/r_bt_s32_b128_sb16
+	--ckpt_dir ../models/r_mm_bt_s128_b128_sb64
 
 python main.py \
 	--mode train \
 	--order instance \
 	--model reservoir_bt \
-	--stm_size 256 \
-	--stm_batch_size 128 \
-	--stm_span 1500 \
+	--stm_size 128 \
+	--stm_batch_size 64 \
+	--stm_span 1000 \
 	--batch_size 128 \
 	--epochs 10 \
 	--warmup_epochs 1 \
@@ -44,8 +48,8 @@ python main.py \
 	--print_freq 400 \
 	--save_freq 5 \
 	--projector 2048-2048 \
-	--save_dir ../models/r_bt_s256_b128_sb128 \
-	--corrupt_rate 0.1 
+	--save_dir ../models/r_bt_span1000
+#	--corrupt_rate 0.1 
 #	--use_boundary
 
 python finetune.py \
@@ -56,4 +60,34 @@ python finetune.py \
 	--decay_epochs 20 42 \
 	--weight_decay 1e-4 \
 	--eval_freq 5 \
-	--ckpt_dir ../models/r_bt_s256_b128_sb128
+	--ckpt_dir ../models/r_bt_span1000
+
+python main.py \
+	--mode train \
+	--order instance \
+	--model reservoir_bt \
+	--stm_size 128 \
+	--stm_batch_size 64 \
+	--stm_span 3000 \
+	--batch_size 128 \
+	--epochs 10 \
+	--warmup_epochs 1 \
+	--learning_rate_weights 0.15 \
+	--learning_rate_biases 0.005 \
+	--lr_decay 1 \
+	--print_freq 400 \
+	--save_freq 5 \
+	--projector 2048-2048 \
+	--save_dir ../models/r_bt_span3000
+#	--corrupt_rate 0.1 
+#	--use_boundary
+
+python finetune.py \
+	--batch_size 256 \
+	--epochs 50 \
+	--learning_rate 0.2 \
+	--lr_decay 0.1 \
+	--decay_epochs 20 42 \
+	--weight_decay 1e-4 \
+	--eval_freq 5 \
+	--ckpt_dir ../models/r_bt_span3000
