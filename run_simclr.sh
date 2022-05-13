faaeb5deec56875b8f12ced2ad6a4e89df28bbbb
SkrighYZ <zhang.yipeng@columbia.edu>
2022-05-13 08:00:51 -0400

    .

--- run_simclr.sh
+++ run_simclr.sh
@@ -1,84 +1,13 @@
 #!/bin/bash
 
 
-python main.py \
-	--mode train \
-	--order instance \
-	--model reservoir_simclr \
-	--stm_size 128 \
-	--stm_batch_size 64 \
-	--stm_span 500 \
-	--batch_size 128 \
-	--epochs 10 \
-	--warmup_epochs 1 \
-	--learning_rate_weights 0.15 \
-	--learning_rate_biases 0.005 \
-	--lr_decay 1 \
-	--print_freq 400 \
-	--save_freq 5 \
-	--projector 2048-2048 \
-	--save_dir ../models/r_simclr_span500
-
-python main.py \
-	--mode train \
-	--order instance \
-	--model reservoir_simclr \
-	--stm_size 128 \
-	--stm_batch_size 64 \
-	--stm_span 1000 \
-	--batch_size 128 \
-	--epochs 10 \
-	--warmup_epochs 1 \
-	--learning_rate_weights 0.15 \
-	--learning_rate_biases 0.005 \
-	--lr_decay 1 \
-	--print_freq 400 \
-	--save_freq 5 \
-	--projector 2048-2048 \
-	--save_dir ../models/r_simclr_span1000
-
-python main.py \
-	--mode train \
-	--order instance \
-	--model reservoir_simclr \
-	--stm_size 128 \
-	--stm_batch_size 64 \
-	--stm_span 3000 \
-	--batch_size 128 \
-	--epochs 10 \
-	--warmup_epochs 1 \
-	--learning_rate_weights 0.15 \
-	--learning_rate_biases 0.005 \
-	--lr_decay 1 \
-	--print_freq 400 \
-	--save_freq 5 \
-	--projector 2048-2048 \
-	--save_dir ../models/r_simclr_span3000
-
-python main.py \
-	--mode train \
-	--order instance \
-	--model reservoir_simclr \
-	--stm_size 128 \
-	--stm_batch_size 64 \
-	--stm_span 2000 \
-	--batch_size 128 \
-	--epochs 10 \
-	--warmup_epochs 1 \
-	--learning_rate_weights 0.15 \
-	--learning_rate_biases 0.005 \
-	--lr_decay 1 \
-	--print_freq 400 \
-	--save_freq 5 \
-	--projector 2048-2048 \
-	--save_dir ../models/r_simclr_span2000
-
-
-
 # python main.py \
-#   --mode train \
-# 	--order iid \
-# 	--model sliding_simclr \
+# 	--mode train \
+# 	--order instance \
+# 	--model reservoir_simclr \
+# 	--stm_size 128 \
+# 	--stm_batch_size 64 \
+# 	--stm_span 500 \
 # 	--batch_size 128 \
 # 	--epochs 10 \
 # 	--warmup_epochs 1 \
@@ -88,7 +17,78 @@ python main.py \
 # 	--print_freq 400 \
 # 	--save_freq 5 \
 # 	--projector 2048-2048 \
-# 	--save_dir ../models/iid_simclr_b128
+# 	--save_dir ../models/r_simclr_span500
+
+# python main.py \
+# 	--mode train \
+# 	--order instance \
+# 	--model reservoir_simclr \
+# 	--stm_size 128 \
+# 	--stm_batch_size 64 \
+# 	--stm_span 1000 \
+# 	--batch_size 128 \
+# 	--epochs 10 \
+# 	--warmup_epochs 1 \
+# 	--learning_rate_weights 0.15 \
+# 	--learning_rate_biases 0.005 \
+# 	--lr_decay 1 \
+# 	--print_freq 400 \
+# 	--save_freq 5 \
+# 	--projector 2048-2048 \
+# 	--save_dir ../models/r_simclr_span1000
+
+# python main.py \
+# 	--mode train \
+# 	--order instance \
+# 	--model reservoir_simclr \
+# 	--stm_size 128 \
+# 	--stm_batch_size 64 \
+# 	--stm_span 3000 \
+# 	--batch_size 128 \
+# 	--epochs 10 \
+# 	--warmup_epochs 1 \
+# 	--learning_rate_weights 0.15 \
+# 	--learning_rate_biases 0.005 \
+# 	--lr_decay 1 \
+# 	--print_freq 400 \
+# 	--save_freq 5 \
+# 	--projector 2048-2048 \
+# 	--save_dir ../models/r_simclr_span3000
+
+# python main.py \
+# 	--mode train \
+# 	--order instance \
+# 	--model reservoir_simclr \
+# 	--stm_size 128 \
+# 	--stm_batch_size 64 \
+# 	--stm_span 2000 \
+# 	--batch_size 128 \
+# 	--epochs 10 \
+# 	--warmup_epochs 1 \
+# 	--learning_rate_weights 0.15 \
+# 	--learning_rate_biases 0.005 \
+# 	--lr_decay 1 \
+# 	--print_freq 400 \
+# 	--save_freq 5 \
+# 	--projector 2048-2048 \
+# 	--save_dir ../models/r_simclr_span2000
+
+
+
+python main.py \
+  --mode train \
+	--order instance \
+	--model sliding_simclr \
+	--batch_size 128 \
+	--epochs 10 \
+	--warmup_epochs 1 \
+	--learning_rate_weights 0.15 \
+	--learning_rate_biases 0.005 \
+	--lr_decay 1 \
+	--print_freq 400 \
+	--save_freq 5 \
+	--projector 2048-2048 \
+	--save_dir ../models/instance_simclr_b128
 
 
 
